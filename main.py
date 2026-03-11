36f6cc4b6c41544160a3e4480388e74b2d2b9dfc
oli-dev-ai <oopyrchal@pm.me>
2026-03-11 20:31:22 +0100

    First working neural network

--- main.py
+++ main.py
@@ -34,13 +34,58 @@ class Creature:
         self.angle = output[1]
         x = math.cos(self.angle) * self.speed
         y = math.sin(self.angle) * self.speed
-        self.position = self.position[0] + x, self.position[1] + y
+        self.position = (self.position[0] + x) % WIDTH, (self.position[1] + y) % HEIGHT
+
 
     def draw(self,surface):
         pg.draw.circle(surface, (255,255,255), (int(self.position[0]), int(self.position[1])), 10)
 
 
+def eval_genomes(genomes, config):
+    pg.display.init()
+    screen = pg.display.set_mode((WIDTH, HEIGHT))
+    clock = pg.time.Clock()
+    running = True
+
+    x = 300
+    y = 400
+    food_positions = (x, y)
+
+    for genome_id, genome in genomes:
+        creature = Creature(genome, config)
+        genome.fitness = 0
+
+        for c in range(200):
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    pg.quit()
+                    return
+
+            screen.fill("purple")
+            creature.update(food_positions)
+            creature.draw(screen)
+            pg.draw.circle(screen, (255, 0, 0), food_positions, 8)
+            pg.display.flip()
+
+            if math.dist(creature.position, food_positions) < 20:
+                genome.fitness = 200 - c
+                break
+
+
+def main():
+    config = neat.Config(neat.DefaultGenome,
+                         neat.DefaultReproduction,
+                         neat.DefaultSpeciesSet,
+                         neat.DefaultStagnation,
+                         'config/neat_config.txt')
+    population = neat.Population(config)
+    population.add_reporter(neat.StdOutReporter(True))
+    stats = neat.StatisticsReporter()
+    population.add_reporter(stats)
+    population.run(eval_genomes, n=50)
+    stats.save_genome_fitness()
 
 
 
-
+if __name__ == '__main__':
+    main()
